e0ec9191bfe4502fa3bff4099418e3cbb440a44f
unknown <felistusobieze@gmail.com>
2021-06-06 15:17:39 +0100

    modified style,index, main.js(added a function to switch class for the navigation items)

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready ( () => {
     $("#about-box, #about-head, #about, .display-emp, #delete-tab, #view-tab").hide();
+    let target;
 
 // codes to execute on page load
     $(window).on("load", () => {
@@ -16,7 +17,7 @@ $(document).ready ( () => {
 
 // change the plus icon to minu and vice versa
     $(".icon").click( (event) => {
-        let target = $( event.target );
+        target = $( event.target );
         if ( target.hasClass("fa-plus") ) {
             target 
                     .removeClass("fa-plus")
@@ -28,9 +29,13 @@ $(document).ready ( () => {
                     .addClass("fa-plus");
             target.parent().siblings().hide()
         }
-    })
+    });
 
-
+// controls the addition of the ".active" to each list item in the nav
+    $(".nav-item").click( function() {
+        $(this).siblings(".nav-item").removeClass("active");
+        $(this).addClass("active")
+    });
 
 
 
