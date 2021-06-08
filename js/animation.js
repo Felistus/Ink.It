3fbbf8df3eca4dd9705ace5ce2b38641e1e3b3d6
unknown <felistusobieze@gmail.com>
2021-06-08 23:30:15 +0100

    added animation to the modal

--- js/animation.js
+++ js/animation.js
@@ -45,15 +45,16 @@ $(document).ready ( () => {
 
     // modal
     new jBox('Modal', {
+        width: 360,
+        theme: "TooltipDark",
+        attach: ".tooltip-icon",
+        content: '<div class="complaint-card" id=""> <h1 id="complaint-head">send a message</h1> <form class="form-block"  id=""> <input type="text" name="first-name" id="complaint-first-name" placeholder="first name" class="complaint-box" required> <input type="text" name="last-name" id="complaint-last-name" placeholder="last name" class="complaint-box" required> <input type="tel" name="phone-num" class="complaint-box" id="complaint-phoneNumber" minlength="11" maxlength="11" required placeholder="phone number"> <textarea name="user-complaint" id="complaint" cols="30" rows="5"></textarea> <div class="btn"> <input type="submit" value="send" class="send-btn" id="send-btn"> </div> </form> </div>',
         position: {
             x: 'left',
             y: 'top'
           },
-        width: 360,
-        theme: "TooltipDark",
-        attach: ".tooltip-icon",
-        content: '<div class="complaint-card" id=""> <h1 id="complaint-head">send a message</h1> <form class="form-block"  id=""> <input type="text" name="first-name" id="complaint-first-name" placeholder="first name" class="complaint-box" required> <input type="text" name="last-name" id="complaint-last-name" placeholder="last name" class="complaint-box" required> <input type="tel" name="phone-num" class="complaint-box" id="complaint-phoneNumber" minlength="11" maxlength="11" required placeholder="phone number"> <textarea name="user-complaint" id="complaint" cols="30" rows="5"></textarea> <div class="btn"> <input type="submit" value="send" class="send-btn" id="send-btn"> </div> </form> </div>'
-      });
+        animation: "tada"
+    });
     // modal end
 // END OF THE JBOX JQUERY PLUGIN
 
