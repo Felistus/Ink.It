28c6cec0ce301cd1c1b47f7cedbcc8706a2fdce9
unknown <felistusobieze@gmail.com>
2021-06-11 21:23:51 +0100

    added the List All Employee function

--- js/main.js
+++ js/main.js
@@ -2,31 +2,57 @@ $(document).ready( () => {
     const userDetails = JSON.parse(localStorage.getItem("User")) || []; //checks if the user array is empty or filled on page load
     let existingUser;
     let count = 0;
-    // $("#close-employees-list").hide();
     let nameCheckReg =  /[^aA-zZ]/g; //regular expression to check if the username contains digits
     let numberCheckReg = /[0-9]/g; //regular expression to check if the user phone number contains digits only or combined and extracts only digits
- 
+    let fullName;
+    let closeBTN = $("#close-emp").hide();
+
+    // EXECUTE ON PAGE LOAD
+    $(window).on("load", () => {
+        listAllEmployees();
+    });
+
  // FUNCTION EXPRESSIONS
      // LOCAL STORAGE SET ITEM METHOD
      let localStorageSetItem = () =>{
          return localStorage.setItem("User", JSON.stringify(userDetails));
      }
-     let reloadPage = () => {
-         return location.reload();
-     }
+    //  let reloadPage = () => {
+    //      return location.reload();
+    //  }
+// CAPITALIZE FIRST LETTER OF EACH WORD GIVEN TWO PARAMETERS
+    let toCapitalize = (first, second) =>{
+        joinedName = first+ " " +second;
+        fullName = joinedName.toLowerCase().split(" ");
+        for (let i = 0; i < fullName.length; i++) {
+            fullName[i] = fullName[i].charAt(0).toUpperCase() + fullName[i].substring(1);
+        }
+        return fullName.join(" ");
+    }
+//  CREATE A LIST OF ALL EMPLOYEES AND APPEND
+    let listAllEmployees = () => {
+        return userDetails.forEach( (element) => {
+            $("#all-employees").append(`
+                    <li class="display-list ">
+                        <span class="userName fontFam">${element.fullName}</span>
+                        <span class="employee-number fontFam"> ${element.userPhoneNumber}</span>
+                        <span class="employee-count fontFam"> ${element.attendanceCount}</span>
+                    </li>`);
+            });
+    };
  // END OF FUNCTION EXPRESSIONS
- 
+     
 // ADMIN PANEL
     // POST REQUEST: CREATE NEW EMPLOYEE
     $("#createUserButton").click((event) => {
         event.preventDefault();
-        const firstName = ( $("#firstName").val() ).toUpperCase();
-        const lastName = ( $("#lastName").val() ).toUpperCase();
+        const firstName = ( $("#firstName").val() );
+        const lastName = ( $("#lastName").val() );
+        const userName = toCapitalize(firstName, lastName);
         const userPhoneNumber = $("#userPhoneNumber").val();                                         
         const password = Math.random().toString(36).slice(2); // generates random password for each user
         const userInfo = {
-            firstName,
-            lastName,
+            fullName: userName,
             userPhoneNumber,
             attendanceCount: count,
             password,
@@ -67,50 +93,48 @@ $(document).ready( () => {
     // END OF POST REQUEST: CREATE NEW EMPLOYEE
 
     // GET REQUEST: LIST ALL EMPLOYEES
-    $("#list-employees-button").click((event) => {
-        event.preventDefault(); 
-        $(".all-employee").remove();
-    
-        for(let index = 0; index < userDetails.length; index++){
-            $("#list-all-employees").append(`
-                    <li class="all-employee list-group-item d-flex justify-content-between align-items-center">
-                        ${userDetails[index].firstName} ${userDetails[index].lastName} 
-                    <span class="badge badge-secondary badge-pill"> ${userDetails[index].phoneNumber}</span>
-                    <span class="badge badge-primary"> ${userDetails[index].attendanceCount}</span>
-                    </li>`);
-        }
-        $("#close-employees-list").show().click(()=>{
-            $(".all-employee").remove();
-            $("#close-employees-list").hide()
-        });
+    $("#list-employees").click(() => {
+        $(".display-list").remove();
+        listAllEmployees();
     });
     // END OF LIST ALL EMPLOYEES GET REQUEST
-});
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // END OF ADMIN PANEL
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
